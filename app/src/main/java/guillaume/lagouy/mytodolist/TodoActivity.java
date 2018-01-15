24e2a76f00f62cca2434ae69fb8fd8290b197927
Guillaume LAGOUY <lagouyg@iut-acy.local>
2018-01-15 10:47:36 +0100

    first commit

---
+++ app/src/main/java/guillaume/lagouy/mytodolist/TodoActivity.java
@@ -0,0 +1,59 @@
+package guillaume.lagouy.mytodolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by lagouyg on 15/01/2018.
+ */
+public class TodoActivity extends Activity implements View.OnClickListener{
+    private Button btnValider;
+    private Button btnVider;
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_layout);
+        btnValider = (Button)findViewById(R.id.buttonValider);
+        btnValider.setOnClickListener(this);
+
+        btnVider = (Button)findViewById(R.id.buttonVider);
+        btnVider.setOnClickListener(this);
+
+        textView = (TextView)findViewById(R.id.textViewId);
+        editText = (EditText)findViewById(R.id.editTextId);
+    }
+    @Override
+    public void onClick(View v){
+        if(v instanceof Button){
+            Button b = (Button)v;
+            if(b == btnValider){
+                Toast.makeText(getApplicationContext(),"Validate", Toast.LENGTH_SHORT).show();
+                Editable item_text = editText.getText();
+                textView.append(item_text+"\n");
+
+            }
+            else if(b == btnVider){
+                Toast.makeText(getApplicationContext(),"Empty list", Toast.LENGTH_SHORT).show();
+                textView.setText("");
+            }
+        }
+    }
+
+    public static final class Singleton{
+        private static volatile Singleton instance = null;
+        private Singleton(){
+            super();
+        }
+    }
+}
